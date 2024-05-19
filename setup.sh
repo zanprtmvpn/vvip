16098017ff41c2cce8cceda09b0e3b01ed7766c8
zanprtmvpn <164328666+zanprtmvpn@users.noreply.github.com>
2024-05-19 12:30:10 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -127,7 +127,7 @@ sts="${Error}"
 fi
 echo -e "\e[32mloading...\e[0m"
 clear
-REPO="https://raw.githubusercontent.com/zanprtmvpn/vvip/main"
+REPO="https://raw.githubusercontent.com/zanprtmvpn/vvip/main/"
 start=$(date +%s)
 secs_to_human() {
 echo "Installation time : $((${1} / 3600)) hours $(((${1} / 60) % 60)) minute's $((${1} % 60)) seconds"
